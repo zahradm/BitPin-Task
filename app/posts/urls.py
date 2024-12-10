63724f8b47e97da6bf6ac1c8a814e65a9c1d01ad
zahra <zh.dehghani741@gmail.com>
2024-12-10 23:43:28 +0330

    refactor urls

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from app.posts.views import PostListView, PostRatingView, PostCreateView
 
 urlpatterns = [
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/<int:post_id>/', PostRatingView.as_view(), name='post-rating'),
-    path('posts/create/', PostCreateView.as_view(), name='post-create'),
+    path('list/', PostListView.as_view(), name='post-list'),
+    path('rate/<int:post_id>/', PostRatingView.as_view(), name='post-rating'),
+    path('create/', PostCreateView.as_view(), name='post-create'),
 ]
