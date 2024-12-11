6e66575ba1595fbcca8bf1bf01c42105523d6a3c
zahra <zh.dehghani741@gmail.com>
2024-12-12 03:01:15 +0330

    refactor average

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,13 +1,17 @@
-from rest_framework.generics import CreateAPIView
-from django.db.models import Avg, Count
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
 from django.core.cache import cache
+from django.db.models import Avg, Count, StdDev
+from django.shortcuts import get_object_or_404
+from django_redis import get_redis_connection
+
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from app.posts.models import Post, Rating
-from app.posts.serializers import PostListSerializer, PostSerializer
 from app.posts.permissions import IsAdminUser
+from app.posts.serializers import PostListSerializer, PostSerializer
 
 
 class PostListView(APIView):
@@ -23,8 +27,16 @@ class PostListView(APIView):
 
         posts = Post.objects.annotate(
             avg_rating=Avg('ratings__score'),
-            rating_count=Count('ratings')
+            rating_count=Count('ratings'),
+            rating_std_dev=StdDev('ratings__score')
         )
+
+        for post in posts:
+            if post.rating_std_dev is not None and post.rating_std_dev < 1:
+                post.avg_weighted_rating = post.avg_rating * 0.5
+            else:
+                post.avg_weighted_rating = post.avg_rating
+
         serializer = PostListSerializer(posts, many=True, context={'request': request})
 
         cache.set(cache_key, serializer.data, cache_timeout)
@@ -36,11 +48,17 @@ class PostRatingView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, post_id):
-        post = Post.objects.get(id=post_id)
+        user = request.user
         score = request.data.get('score')
+        post = get_object_or_404(Post, id=post_id)
 
         if score is None or not (0 <= score <= 5):
-            return Response({'detail': 'Invalid score. It should be between 0 and 5.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'detail': 'Invalid score. It should be between 0 and 5.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if self.is_rate_limited(user, post_id):
+            return Response({'detail': 'You have already rated this post recently.'},
+                            status=status.HTTP_429_TOO_MANY_REQUESTS)
 
         rating, created = Rating.objects.update_or_create(
             post=post,
@@ -51,6 +69,15 @@ class PostRatingView(APIView):
         cache.delete("post_list_with_ratings")
 
         return Response({'detail': 'Rating updated successfully.'}, status=status.HTTP_200_OK)
+
+
+    def is_rate_limited(self, user, post_id):
+        redis = get_redis_connection('default')
+        key = f"rate_limit_{post_id}_user_{user.id}"
+        if redis.get(key):
+            return True
+        redis.setex(key, 60, "1")
+        return False
 
 
 class PostCreateView(CreateAPIView):
